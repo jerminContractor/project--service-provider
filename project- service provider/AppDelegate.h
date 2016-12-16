2e396cc92dc935fdf8898407e197d00b298ce1d5
MACOS <macos@macoss-imac-7.local>
2016-12-16 11:04:44 +0530

    Initial Commit

---
+++ project- service provider/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  project- service provider
+//
+//  Created by MACOS on 12/16/16.
+//  Copyright © 2016 MACOS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ project- service provider/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  project- service provider
+//
+//  Created by MACOS on 12/16/16.
+//  Copyright © 2016 MACOS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
